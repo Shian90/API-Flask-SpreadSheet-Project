b8b0ed592733260dcd45e572cc26eb01a8dc381b
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-01-12 00:47:00 +0600

    Some more modifications

--- functions/makeMessagefunction.py
+++ functions/makeMessagefunction.py
@@ -8,23 +8,26 @@ from functions.getProductsfunction import getProducts
 
 
 def makeMessage(dataframe, name):
-    sum = getTotalPrice(dataframe)
-    adv = getAdvance(dataframe)
-    wc = getWeightCharge(dataframe)
-    deliveryCharge = getDeliveryCharge(dataframe, sum, adv)
-    contactNumber = getContactNumber(dataframe)
-    address = getAddress(dataframe)
+  sum = getTotalPrice(dataframe)
+  adv = getAdvance(dataframe)
+  wc = getWeightCharge(dataframe)
+  deliveryCharge = getDeliveryCharge(dataframe, sum, adv)
+  deliveryArea = ""
+  if ( deliveryCharge > 100 ):
+    deliveryArea = "Outside Dhaka"
+  else:
+    deliveryArea = "Inside Dhaka"
+  contactNumber = getContactNumber(dataframe)
+  address = getAddress(dataframe)
 
-    message = name + "\n"
-    message = getProducts(dataframe, message)
-    message += "\nTotal - " + str(sum) + " BDT\n"
-    message += "Weight charge - " + str(wc) + " BDT\n"
-    message += "Advanced - " + str(adv) + " BDT\n"
-    message += "Due - " + \
-        str(sum - adv + wc) + " BDT + Delivery Charge - " + \
-        str(deliveryCharge) + " BDT\n"
-    message += "\nDelivery Details:\n"
-    message += "Contact Number: " + contactNumber + "\n"
-    message += "Address: " + address + "\n"
-    message += "\n"
-    return message
+  message = name + "\n"
+  message = getProducts(dataframe, message)
+  message+="\nTotal - " + str(sum) + " BDT\n"
+  message+="Weight charge - " + str(wc) + " BDT\n"
+  message+="Advanced - " + str(adv) + " BDT\n"
+  message += "Due - " + str(sum - adv + wc) + " BDT + Delivery Charge - " + str(deliveryCharge) + " BDT (" + deliveryArea + ")\n"
+  message += "\nDelivery Details:\n"
+  message+="Contact Number: " + contactNumber + "\n"
+  message+="Address: " + address + "\n"
+  message += "\n"
+  return message
